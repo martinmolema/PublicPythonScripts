a8f9d9348e0bb90815c41ab872bbe470bee5c68f
Martin Molema <martin@molema.org>
2021-06-12 19:09:50 +0200

    Improvements to make it more OS-independent.

--- DrawIOexporter/draw-io-export.py
+++ DrawIOexporter/draw-io-export.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 
+################################################################################################################################
+# Author  : Martin Molema (martin@molema.org / martin.molema@nhlstenden.com)
+# Goal    : Export all diagrams from a given Diagrams.net / DrawIO file.
+# Version : 1
+#
+# Requires:
+# - Python: xml.etree.ElementTree (https://docs.python.org/3/library/xml.etree.elementtree.html)
+# - Installed version of DrawIO (offline) See http://www.diagrams.net / https://github.com/jgraph/drawio-desktop
+#
+################################################################################################################################
+
 # Example usage: draw-io-export.py --scale=2 --format=PNG --output-directory=images -t Diagrams.drawio
+
 import collections
-import os
+import subprocess
 import xml.etree.ElementTree as ET
 import getopt, sys
+import os.path
+
+drawIoExecutable="drawio"
 
 # First setup the options in a generic way
 OptionInfo = collections.namedtuple('OptionInfo', 'short, long, hasParameter, parameterName, usage')
@@ -27,12 +42,14 @@ basename = ""
 inputFullPath = ""
 format = "PNG"
 scale = "1"
-directory = "./"
+directory = f".{os.path.sep}exportedPages"
 transparent = False
 
 # get the name of the executed script from the command line options (remove the preceding path if present)
 helpCommandLine = os.path.basename(sys.argv[0]) + " "
 
+# Now convert the list of options to an string that can be parsed by GetOpts; in the meanwhile construct the USAGE
+# text.
 for opt in optionlist:
     # destruct the tuple to separate parameters
     short, long, hasParameter, parameterName, usage = opt
@@ -117,9 +134,24 @@ except getopt.error as err:
     print(str(err))
     exit(2)
 
+# Check if the directory ends with a forward slash
+if directory[-1:] != os.path.sep:
+    directory = f"{directory}{os.path.sep}"
+
+if directory[1:1] != os.path.sep:
+    directory = f"{os.getcwd()}{os.path.sep}{directory}"
+
 print(("Using input file %s") % (inputFullPath))
 print(("Using output base filename %s") % (basename))
 print(("Sending result files to directory %s") % (directory))
+
+if not os.path.exists(directory):
+    try:
+        print(f"Destination path does not exist. Creating {directory}")
+        os.makedirs(directory)
+    except:
+        print(f"Could not create path! {directory}")
+        sys.exit(1)
 
 root = ET.parse(inputFullPath).getroot()
 pageNumber = 0
@@ -129,20 +161,37 @@ for diagram in root.findall('diagram'):
     pagename = diagram.get('name')
     print("Processing " + pagename)
 
-    # construct a new filename
-    newfilename = directory + "/" + basename + ' - ' + pagename + '.png'
+    # construct a new filename (directory already ends with OS-path separator char
+    newfilename = f"{directory}{basename} - {pagename}.png"
 
-    # construct a command line. assume that drawio can be run using the path-variable
-    commandline = "drawio -x -p "  + str(pageNumber) + " -o '" + newfilename + "' -f " + format + " -s " + scale
+    # construct a command line. assume that drawio can be run using the path-variable;
+    # setup the commandline as an array of parameters; options that have parameters will be separated into two parts
+    # escaping spaces for filenames is not necessary
+    commandline = []
+    commandline.append(drawIoExecutable)
+    commandline.append(f"-x")
+    commandline.append(f"-p")
+    commandline.append(f"{pageNumber}")
+    commandline.append(f"-o")
+    commandline.append(f"{newfilename}")
+    commandline.append(f"-f")
+    commandline.append(f"{format}")
+    commandline.append(f"-s")
+    commandline.append(f"{scale}")
     if transparent:
-        commandline += " -t"
+        commandline.append(f"-t")
 
     # add the filename as the last parameter
-    commandline = commandline + " " + inputFullPath
+    commandline.append(inputFullPath)
 
-    # print (commandline)
-    result = os.system(commandline)
-    if result != 0:
-        print("#Error running command line")
+    try:
+        print(f"Using working directory {os.getcwd()}")
+        result = subprocess.run(commandline, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        print(result.stdout.decode('utf-8'))
+    except:
+        print("Error running command line")
+        print(result.stderr.decode('utf-8'))
+        sys.exit(1)
+
     pageNumber += 1
 
